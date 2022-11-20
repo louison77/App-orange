12db318ab5aae928a500a2207b55f2195070c20f
Louison <Louison7777@gmail.com>
2022-11-20 21:05:05 +0100

    premier envoi sur internet

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar1 from '../components/Navbar1';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar1/>
+        </div>
+    );
+};
+
+export default Home;
