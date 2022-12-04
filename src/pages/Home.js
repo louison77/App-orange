59f6b7fc0183173b1374c1ee5d2ecde9044a14f8
Louison <Louison7777@gmail.com>
2022-12-04 18:29:34 +0100

    test louison

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar1 from '../components/Navbar1';
 import Navigation from '../components/Navigation';
+import Accueil from '../components/Accueil';
 
 const Home = () => {
     return (
         <div>
             <Navbar1/>
+            <Accueil/>
         </div>
     );
 };
