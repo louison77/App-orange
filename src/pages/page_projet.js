70aa5b21051e8437172d1a039e71b6d8bea2cc9e
Louison <Louison7777@gmail.com>
2022-12-20 16:01:27 +0100

    add fichier config

---
+++ src/pages/page_projet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page_projet = () => {
+    return (
+        <div>
+            <Navbar1/>
+            
+        </div>
+    );
+};
+
+export default page_projet;
