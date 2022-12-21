37162adbd88902495b93b9bdf873bddd0206b678
Louison <Louison7777@gmail.com>
2022-12-21 16:47:33 +0100

    Commit made from VS Code Azure Static Web Apps

---
+++ src/components/Navbar1.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/components/_accueil.scss'
+
+const Navbar1 = () => {
+    return (
+        <div className='navbar1'>
+            <div className='leftpart'>
+                <img src="./logoorange.jpg" className='logoorange' alt="logo orange" />
+                <ion-icon classname='btnhome' name="home-outline"></ion-icon>
+                
+            </div> 
+            <div className='rightpart'>
+                <h3>Navigation</h3>
+                <div className='chevron'>
+                    <ion-icon name="chevron-down-outline" ></ion-icon>
+                </div>
+                <div className='profil'>
+                    <ion-icon name="person-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar1;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar1 from '../components/Navbar1';
+import Navigation from '../components/Navigation';
+import Accueil from '../components/Accueil';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar1/>
+            <Accueil/>
+        </div>
+    );
+};
+
+export default Home;
